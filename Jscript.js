ce3fe9780359d9a34683f26b990461951d7671ac
1BObC <bob.cocksedge@gmail.com>
2024-03-24 22:49:35 +0000

    populate files with boilerplate and add first exercise

--- Jscript.js
+++ Jscript.js
@@ -1,1 +1,14 @@
 // js nested function exercises only
+// Exercise from javascript.info
+function sayHiBye(firstName, lastName) {
+
+    // helper nested function to use below
+    function getFullName() {
+      return firstName + " " + lastName;
+    }
+  
+    alert( "Hello, " + getFullName() );
+    alert( "Bye, " + getFullName() );  
+  }
+
+  sayHiBye("Damon", "Runyon");
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 1jsNest
 Exercise repo exclusively JS nests
 "Cre ate files for 1 js Nest"
-"populate files with boilerplate"
+"populate files with boilerplate and add first exercise"
